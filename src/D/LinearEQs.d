510cba71a366934b563ab816dbd5e54739abba25
maisvendoo <maisvendoo@gmail.com>
2015-01-20 20:33:50 +0300

    Added Gauss method with full find leading element

--- src/D/LinearEQs.d
+++ src/D/LinearEQs.d
@@ -10,7 +10,6 @@ import	std.math;
 import	std.stdio;
 
 import	matrix;
-//import	MathFuncs;
 
 //-------------------------------------------------------------------
 //
@@ -108,7 +107,7 @@ void gauss_solver(double[][] A, double[][] b, ref double[] x)
 //-------------------------------------------------------------------
 //
 //-------------------------------------------------------------------
-void gaussLE_solver(double[][] A, double[][] b, ref double[] x)
+void gaussLER_solver(double[][] A, double[][] b, ref double[] x)
 {
 	double	c = 0;
 	int		n = cast(int) A.length;
@@ -155,5 +154,120 @@ void gaussLE_solver(double[][] A, double[][] b, ref double[] x)
 			sum -= A[i][j]*x[j];
 		
 		x[i] = sum / A[i][i];
+	}
+}
+
+//-------------------------------------------------------------------
+//
+//-------------------------------------------------------------------
+void gaussLEF_solver(double[][] A, double[][] b, ref double[] x)
+{
+	int			n = cast(int) A.length;
+	int			p = 0;
+	int 		q = 0;
+	double		c = 0;
+	double		m = 0;
+	int			count = 0;
+	int[]		unkw_num = new int[n];
+	double		eps_z = 1e-10;
+
+	// Choose leading element
+	double get_LE(double[][] M, ref int p, ref int q)
+	{
+		double max = 0;
+		int n = cast(int) M.length;
+
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				if (abs(M[i][j]) > max)
+				{
+					max = abs(M[i][j]);
+					p = i;
+					q = j;
+				}
+			}
+		}
+
+		return M[p][q];
+	}
+
+	// Tmp matrixes
+	double[][] A1 = create_matrix(n, n+1);
+	double[][] A2 = create_matrix(n, n+1);
+	double[][] A3 = create_matrix(n, n+1);
+
+	// Prepare tmp matrixes
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			A1[i][j] = A[i][j];
+			A2[i][j] = A[i][j];
+		}
+
+		A1[i][n] = b[i][0];
+		A2[i][n] = b[i][0];
+	}
+
+	// Elimination of unknowns with pivoting
+	do
+	{
+		c = get_LE(A1, p, q);
+
+		for (int i = 0; i < n; i++)
+		{
+			if (i != p)
+			{
+				m = -(A1[i][q] / A1[p][q]);
+
+				rows_sum(A1, 1, m, i, p);
+				rows_sum(A2, 1, m, i, p);
+
+				A[i][q] = 0;
+			}		
+		}	
+
+		row_zero(A1, p);
+		A3[count] = A2[p];
+
+		count++;
+
+	} while (count <= n-1);
+
+	// Transorm matrix to triangle form
+	for (int i = 0; i < n; i++)	
+		unkw_num[i] = i;
+
+	for (int i = n-1; i >= 0; i--)
+	{
+		if (abs(A3[i][i]) > eps_z)
+			continue;
+
+		for (int k = 0; k < i; k++)
+		{
+			if (abs(A3[i][k]) > eps_z)
+			{
+				p = k;
+			}
+		}
+
+		columns_change(A3, i, p);
+		change(unkw_num, i, p);
+	}
+
+	// Backward (roots found)
+	double[] y = new double[n];
+
+	for (int i = n-1; i >=0; i--)
+	{
+		double sum = A3[i][n];
+		
+		for (int j = n-1; j >= i+1; j--)
+			sum -= A3[i][j]*y[j];
+		
+		y[i] = sum / A3[i][i];
+		x[unkw_num[i]] = y[i];
 	}
 }
