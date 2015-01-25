93d3277f7069ab55b46df3630ef80ead6db118c3
maisvendoo <maisvendoo@gmail.com>
2015-01-25 14:48:05 +0300

    Added set Lua-script name from command line

--- src/D/main.d
+++ src/D/main.d
@@ -6,21 +6,29 @@
 //-------------------------------------------------------------------
 module	main;
 
+import	std.stdio;
+
 import	Train;
 
 //-------------------------------------------------------------------
 //
 //-------------------------------------------------------------------
-void main()
+void main(string[] args)
 {
+	// Get config Lua script name from command line
+	string	cfg_script = args[1];
+
 	// Model creation
 	CTrainModel model = new CTrainModel();
 
 	// Model initialization
-	int err = model.init("../../cfg/train.lua");
+	int err = model.init(cfg_script);
 
 	if (err == -1)
+	{
+		writeln("FAIL: train model is't inizialized");
 		return;
+	}
 
 	model.start();
 }
