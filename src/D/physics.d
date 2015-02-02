03dfee3846ff663687dba5a485b7b085ee2bd19f
maisvendoo <maisvendoo@gmail.com>
2015-02-02 22:52:28 +0300

    Some constants are added

--- src/D/physics.d
+++ src/D/physics.d
@@ -6,7 +6,14 @@
 //-------------------------------------------------------------------
 module physics;
 
+//-------------------------------------------------------------------
+//		Some physical and measurement constants
+//-------------------------------------------------------------------
 enum	double		g		= 9.81;
 enum	double		kmh		= 3.6;
 enum	double		km		= 1000;
 enum	double		TONN	= 1000;
+enum	double		kN		= 1000;
+
+//	Program "zero" (if value less than ZERO - it is zero)
+enum	double		ZERO	= 1e-10;
